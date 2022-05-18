51e6b2113ff7a0ca87feb51203d3cb00b0b4657b
DimaSoroko <84499772+DimaSoroko@users.noreply.github.com>
2022-05-18 17:23:40 +0200

    Add files via upload

---
+++ get_next_line_utils.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsoroko <dsoroko@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 18:10:16 by dsoroko           #+#    #+#             */
+/*   Updated: 2022/05/18 16:47:02 by dsoroko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = malloc(sizeof(char));
+		if (!s1 || !s2)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str)
+	{
+		i = -1;
+		j = -1;
+		while (s1[++i])
+			str[i] = s1[i];
+		while (s2[++j])
+			str[i++] = s2[j];
+		str[i] = '\0';
+	}
+	free (s1);
+	return (str);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (0);
+}
